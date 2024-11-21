214018504d426b2a9c1f86800cfed5215f7b875b
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-11-21 14:01:37 +0800

    feat:introduce given student

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,4 +1,17 @@
 package oo;
 
 public class Student {
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    public Student(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old. I am a student.";
+    }
 }
