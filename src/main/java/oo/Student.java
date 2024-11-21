6ea9b04288ab874c6703bf6808b5b6c547c41174
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-11-21 14:07:30 +0800

    refactor:Student extends Person

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -2,31 +2,14 @@ package oo;
 
 import java.util.Objects;
 
-public class Student {
-    private Integer id;
-    private String name;
-    private Integer age;
+public class Student extends Person{
+
 
     public Student(Integer id, String name, Integer age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
+        super(id, name, age);
     }
 
     public String introduce() {
-        return "My name is " + name + ". I am " + age + " years old. I am a student.";
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return Objects.equals(id, student.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
+        return super.introduce()+" I am a student.";
     }
 }
