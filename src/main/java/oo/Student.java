ff35f47728a548229f2426413c9b965fb407643d
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-11-22 00:10:29 +0800

    refactor:reformat

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -3,17 +3,16 @@ package oo;
 public class Student extends Person {
     private Klass klass;
 
-
     public Student(Integer id, String name, Integer age) {
         super(id, name, age);
     }
 
     public String introduce() {
-        if(this.klass == null)
+        if (this.klass == null)
             return super.introduce() + " I am a student.";
-        if(this.klass.isLeader(this))
-            return super.introduce() + " I am a student. I am the leader of class " + this.klass.getNumber()+".";
-        return super.introduce() + " I am a student. I am in class " + this.klass.getNumber()+".";
+        if (this.klass.isLeader(this))
+            return super.introduce() + " I am a student. I am the leader of class " + this.klass.getNumber() + ".";
+        return super.introduce() + " I am a student. I am in class " + this.klass.getNumber() + ".";
     }
 
     public void join(Klass klass) {
@@ -21,7 +20,7 @@ public class Student extends Person {
     }
 
     public boolean isIn(Klass klass) {
-        if(this.klass == null)
+        if (this.klass == null)
             return false;
         return this.klass.equals(klass);
     }
