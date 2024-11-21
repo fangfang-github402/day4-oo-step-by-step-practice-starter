3483b0eb0b3332a999fae26d9c2eea8bc6e77c9e
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-11-21 14:39:38 +0800

    feat:add techClasses attributes & assignTo method & belongsTo method

--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -1,6 +1,11 @@
 package oo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Teacher extends Person{
+    List<Klass> techClasses = new ArrayList<>();
+
     public Teacher(Integer id, String name, Integer age) {
         super(id, name, age);
     }
@@ -8,5 +13,13 @@ public class Teacher extends Person{
     @Override
     public String introduce() {
         return super.introduce()+" I am a teacher.";
+    }
+
+    public void assignTo(Klass klass){
+        techClasses.add(klass);
+    }
+
+    public boolean belongsTo(Klass klass) {
+        return techClasses.contains(klass);
     }
 }
