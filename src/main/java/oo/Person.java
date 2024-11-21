62736128c6d776e89d66a61e07b093f560087851
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-11-21 13:41:09 +0800

    feat:introduce given person

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,4 +1,17 @@
 package oo;
 
 public class Person {
+    private  Integer id;
+    private  String name;
+    private  Integer age;
+
+    public Person(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce(){
+        return "My name is " + name + ". I am " + age +" years old.";
+    }
 }
